1d74df8e3e06bae70adf8eb05175dd6df2024efb
Gabriele Repinoski <gabrielerepinoski@gmail.com>
2024-09-14 22:28:36 -0300

    Descubra o número
    Mini jogo onde você deve tentar acertar o número.

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+print('Bem vindo ao jogo!')
+def descubra():
+    num= random.randint(1,100)
+    while True:
+        tentativa= int(input('Tente acertar o número: '))
+
+        if tentativa == num:
+            print("Parabéns, você acertou o número")
+            break
+        elif tentativa > num:
+            print("Seu palpite foi muito alto")
+        elif tentativa < num:
+            print("Seu palpite foi muito baixo")
+        else:
+            print("Você errou o número, tente mais uma vez")
+
+
+if __name__ == '__main__':
+    descubra()
