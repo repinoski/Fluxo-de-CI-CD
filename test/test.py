96e6add2ac3cbfe925993f707b1b77160ed4f7aa
Gabriele Repinoski <gabrielerepinoski@gmail.com>
2024-09-30 15:50:24 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,4 +5,14 @@ from main import *
 def test_acertar_primeira_tentativa():
     with patch ('random.randint', return_value=42):
         resultado = descubra()
-    assert resultado == "Parabéns, você acertou o número"
+    assert resultado == {"Parabéns, você acertou o número": True, 'num': 42}
+
+def test_numero_invalido():
+    with pytest.raises(ValueError):
+            descubra()
+
+    with pytest.raises(ValueError):
+            descubra("vinte")
+
+    with pytest.raises(ValueError):
+            descubra(3.14)
