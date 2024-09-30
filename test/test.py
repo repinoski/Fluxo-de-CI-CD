730c3bddd3ad529a3b2c7eb57f6c9cd1777d33b0
Gabriele Repinoski <gabrielerepinoski@gmail.com>
2024-09-30 16:03:11 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,18 +1,15 @@
 import pytest
 from unittest.mock import patch
+from io import StringIO
 from main import *
 
 def test_acertar_primeira_tentativa():
-    with patch ('random.randint', return_value=42):
+    with patch ('random.randint', return_value=42), \
+         patch ('sys.stdin', new=StringIO ('42')):
         resultado = descubra()
-    assert resultado == {"Parabéns, você acertou o número": True, 'num': 42}
+    assert resultado == "Parabéns, você acertou o número"
 
 def test_numero_invalido():
-    with pytest.raises(ValueError):
+    with patch('sys.stdin', new=StringIO('dois')):
+        with pytest.raises(ValueError):
             descubra()
-
-    with pytest.raises(ValueError):
-            descubra("vinte")
-
-    with pytest.raises(ValueError):
-            descubra(3.14)
